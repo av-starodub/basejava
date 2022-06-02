910cef207f43a3e0f1a5420cf6644b92a577e51a
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-06-02 20:52:19 +0300

    Implement class Item.

--- src/ru/javawebinar/basejava/model/item/Item.java
+++ src/ru/javawebinar/basejava/model/item/Item.java
@@ -1,4 +1,60 @@
 package ru.javawebinar.basejava.model.item;
 
+import ru.javawebinar.basejava.model.enumKeyTypes.HeaderType;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * The class to store information about an organisation in a person's career.
+ */
 public class Item {
+    private final Header header;
+    private final List<Info> informationBlocks;
+
+    public Item(EnumMap<HeaderType, String> header, List<Info> info) {
+        this.header = new Header() {{
+            addAll(Objects.requireNonNull(header));
+        }};
+        informationBlocks = new ArrayList<>(Objects.requireNonNull(info));
+    }
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public List<Info> getInfo() {
+        return List.copyOf(informationBlocks);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        if (!header.equals(item.header)) return false;
+        return informationBlocks.equals(item.informationBlocks);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = header.hashCode();
+        result = 31 * result + informationBlocks.hashCode();
+        return result;
+    }
+
+    private String toStringListInfo() {
+        StringBuilder info = new StringBuilder();
+        informationBlocks.forEach(i -> info.append(i).append("\n"));
+        return info.toString();
+    }
+
+    @Override
+    public String toString() {
+        return header + toStringListInfo();
+    }
 }
--- src/ru/javawebinar/basejava/modelDataTest/testData/Experience.java
+++ src/ru/javawebinar/basejava/modelDataTest/testData/Experience.java
@@ -1,12 +1,36 @@
 package ru.javawebinar.basejava.modelDataTest.testData;
 
+import ru.javawebinar.basejava.model.enumKeyTypes.HeaderType;
+import ru.javawebinar.basejava.model.enumKeyTypes.InfoType;
+import ru.javawebinar.basejava.model.item.Info;
+import ru.javawebinar.basejava.model.item.Item;
 import ru.javawebinar.basejava.model.sections.ListItemSection;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
+
+import static ru.javawebinar.basejava.model.enumKeyTypes.HeaderType.*;
+import static ru.javawebinar.basejava.model.enumKeyTypes.InfoType.*;
 
 public class Experience {
 
     public static ListItemSection createSection() {
-        return new ListItemSection(new ArrayList<>());
+        return new ListItemSection(new ArrayList<>() {{
+            add(new Item(
+                    new EnumMap<>(HeaderType.class) {{
+                        put(TITLE, "Java Online Projects");
+                        put(LINK, "https://javaops.ru/");
+                    }},
+                    new ArrayList<>() {{
+                        add(new Info() {{
+                            addAll(new EnumMap<>(InfoType.class) {{
+                                put(START, "10/2013");
+                                put(END, "Сейчас");
+                                put(HEADER, "Автор проекта");
+                                put(DESCRIPTION, "Создание, организация и проведение Java онлайн проектов и стажировок.");
+                            }});
+                        }});
+                    }}));
+        }});
     }
 }
