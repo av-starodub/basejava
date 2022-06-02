20c0d50b2bc2b94096e68f0a2b33462562ffbafa
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-06-02 15:25:25 +0300

    Implement interface Chapter.

--- src/ru/javawebinar/basejava/model/chapters/AbstractEnumChapter.java
+++ src/ru/javawebinar/basejava/model/chapters/AbstractEnumChapter.java
@@ -2,5 +2,23 @@ package ru.javawebinar.basejava.model.chapters;
 
 import ru.javawebinar.basejava.model.interfaces.Chapter;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
+
 public class AbstractEnumChapter<K extends Enum<K>, V> implements Chapter<K, V> {
+    @Override
+    public String getTitle(K key) {
+        return null;
+    }
+
+    @Override
+    public void addAll(EnumMap<K, V> items) {
+
+    }
+
+    @Override
+    public Set<Map.Entry<K, V>> getAll() {
+        return null;
+    }
 }
--- src/ru/javawebinar/basejava/model/interfaces/Chapter.java
+++ src/ru/javawebinar/basejava/model/interfaces/Chapter.java
@@ -1,4 +1,13 @@
 package ru.javawebinar.basejava.model.interfaces;
 
-public interface Chapter <K extends Enum<K>, V> {
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
+
+public interface Chapter<K extends Enum<K>, V> {
+    String getTitle(K key);
+
+    void addAll(EnumMap<K, V> items);
+
+    Set<Map.Entry<K, V>> getAll();
 }
