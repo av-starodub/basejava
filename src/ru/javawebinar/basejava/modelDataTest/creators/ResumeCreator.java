8689e4239cbfc2da100412aad07842b77e006355
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-06-19 15:48:48 +0300

    Implement method createResume(String uuid, String fullName) and apply it in AbstractStorageTest.

--- src/ru/javawebinar/basejava/modelDataTest/creators/ResumeCreator.java
+++ src/ru/javawebinar/basejava/modelDataTest/creators/ResumeCreator.java
@@ -17,4 +17,15 @@ public class ResumeCreator {
             }});
         }};
     }
+    public static Resume createResume(String uuid, String fullName) {
+        return new Resume(uuid, fullName) {{
+            setContacts(new EnumMap<>(ContactType.class) {{
+                ContactsCreator.create().forEach(contact -> this.put(contact.getKey(), contact.getValue()));
+            }});
+            setSections(new EnumMap<>(SectionType.class) {{
+                SectionsCreator.create().forEach(section -> this.put(section.getKey(), section.getValue()));
+            }});
+        }};
+    }
+
 }
