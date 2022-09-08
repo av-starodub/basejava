898fb7cb81b9568033c40b55da48d49d7ef67a15
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-09-08 16:07:10 +0300

    Connect Postgres to project. Implement scripts to create tables: resume and contact.

---
+++ config/init_db.sql
@@ -0,0 +1,20 @@
+-- auto-generated definition
+create table resume
+(
+    uuid      char(36) not null primary key,
+    full_name text     not null
+);
+
+alter table resume owner to postgres;
+
+-- auto-generated definition
+create table contact
+(
+    id          serial primary key,
+    resume_uuid char(36) not null references resume (uuid) on delete cascade,
+    type        text     not null,
+    value       text     not null
+);
+create unique index contact_uuid_type_index on contact (resume_uuid, type);
+
+alter table contact owner to postgres;
