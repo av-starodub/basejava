4efb269880ae53beaa0276cf6b2d51fff9bf47f4
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-09-10 14:18:46 +0300

    Fix per code review: create a Storage variable in the Config class.

--- test/ru/javawebinar/basejava/storage/sqlStorage/SqlStorageTest.java
+++ test/ru/javawebinar/basejava/storage/sqlStorage/SqlStorageTest.java
@@ -1,13 +1,10 @@
 package ru.javawebinar.basejava.storage.sqlStorage;
 
+import ru.javawebinar.basejava.Config;
 import ru.javawebinar.basejava.storage.AbstractStorageTest;
 
 public class SqlStorageTest extends AbstractStorageTest {
     public SqlStorageTest() {
-        super(new SqlStorage(
-                "jdbc:postgresql://localhost:5432/resumes",
-                "postgres",
-                "postgres")
-        );
+        super(Config.get().getStorage());
     }
 }
